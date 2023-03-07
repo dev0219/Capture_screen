00f9e31f6be5c05873d518eeb58b743ce51c4e7b
Imad Jeghalef <jose.bran86@gmail.com>
2023-03-07 12:28:40 -0500

    first commit

---
+++ desktopCaptuer.js
@@ -0,0 +1,42 @@
+﻿chrome.windows.getCurrent({}, w => {
+  chrome.windows.update(w.id, { focused: true }, () => {
+    document.getElementById("captuer").onclick = () => {
+      const sources = ["window", "tab"];
+      chrome.tabs.getCurrent((tab) => {
+        chrome.desktopCapture.chooseDesktopMedia(sources, (streamId) => {
+          console.log("---stream id", streamId)
+          let track, canvas;
+          navigator.mediaDevices.getUserMedia({
+            video: {
+              mandatory: {
+                chromeMediaSource: "desktop",
+                chromeMediaSourceId: streamId
+              },
+            }
+          }).then((stream) => {
+            track = stream.getVideoTracks()[0];
+            const imageCapture = new ImageCapture(track);
+            return imageCapture.grabFrame();
+          }).then((bitmap) => {
+            track.stop();
+            canvas = document.createElement("canvas");
+            canvas.width = bitmap.width;
+            canvas.height = bitmap.height;
+            let context = canvas.getContext("2d");
+            context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
+            return canvas.toDataURL();
+          }).then((url) => {
+            chrome.downloads.download({
+              filename: "screenshot.png",
+              url: url,
+            }, () => {
+              canvas.remove();
+            });
+          }).catch((err) => {
+            console.log(err);
+          })
+        });
+      });
+    }
+  });
+});
---
+++ popup.js
@@ -0,0 +1,7 @@
+﻿const createDate = {
+  url: "desktopCaptuer.html",
+  type: "popup",
+  width: 800,
+  height: 600
+};
+chrome.windows.create(createDate);
